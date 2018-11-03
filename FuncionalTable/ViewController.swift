0c474f668fac7dd77741d81321389a0476725be9
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-11-02 22:54:59 -0300

    Initial Commit

---
+++ FuncionalTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FuncionalTable
+//
+//  Created by Luis Herrera Lillo on 02-11-18.
+//  Copyright © 2018 Luis Herrera Lillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
